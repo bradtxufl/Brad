bf6c0c5c6319917bec49b549d8b6960f9781b519
bradtxufl <brad.w@utexas.edu>
2019-06-03 09:41:28 -0700

    added contact page

--- scripts.js
+++ scripts.js
@@ -3,17 +3,23 @@ var toptrigger = document.querySelector('#navCONTACT')
 var bottomtrigger = document.querySelector('#CONTACT')
 var closeButton = document.querySelector('.close-button')
 
-function toggleModal(e) {
+function addModal(e) {
   e.preventDefault()
-  modal.classList.toggle('show-modal')
+  modal.classList.add('show-modal')
+}
+
+function removeModal(e) {
+  e.preventDefault()
+  modal.className ='modal'
 }
 
 function windowOnClick(event) {
   if (event.target === modal) {
-    toggleModal()
+    removeModal()
   }
 }
 
-toptrigger.addEventListener('click', toggleModal);
-bottomtrigger.addEventListener('click', toggleModal);
+toptrigger.addEventListener('click', addModal);
+bottomtrigger.addEventListener('click', addModal);
 window.addEventListener('click', windowOnClick);
+closeButton.addEventListener('click', removeModal);
